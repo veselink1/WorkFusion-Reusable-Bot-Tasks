a3eb0416fb7d673fa9d83817a437de37027ad90a
Veselin Karaganev <vesko.karaganev@gmail.com>
2019-08-13 22:30:36 +0300

    Update call-webharvest-from-script.groovy

--- scripts/call-webharvest-from-script.groovy
+++ scripts/call-webharvest-from-script.groovy
@@ -3,33 +3,33 @@
  * @param functionName The name of the WebHarvest function to call. 
  * @param params A map of String -> Object with context variables for the function.
  */
-callFunction = { String functionName, Map<String, Object> params = [:] ->
-    StringBuilder sb = new StringBuilder();
-    sb.append("""<call name="${functionName}">""");
-    params.each { key, value ->
-        if (!(key instanceof String)) {
-            throw new IllegalArgumentException("The parameter name '${key}' is not of type String!")
-        }
-        if (!(value instanceof CharSequence) && !(value instanceof Number) && !(value instanceof Boolean)) {
-            throw new IllegalArgumentException("The value of parameter '${key}' is neither a CharSequence nor a primitive type!");
-        }
-        sb.append('<call-param name="');
-        sb.append(key);
-        sb.append('"><![CDATA').append('[');
-        sb.append(value);
-        sb.append(']]').append('></call-param>');
-    };
-    sb.append('</call>');
-    String generated = sb.toString();
-    
-    def node = new org.webharvest.definition.CallDef(
-        org.webharvest.definition.XmlNode.getInstance(
-            new org.xml.sax.InputSource(new java.io.StringReader(generated))));
-    
-    try {
-        return sys.scraper.execute(Arrays.asList(node));
+callFunction = { String functionName, HashMap<String, Object> functionParams -> 
+    org.webharvest.runtime.Scraper scraper = sys.scraper;
+    org.webharvest.definition.ScraperConfiguration configuration = scraper.getConfiguration();
+
+    org.webharvest.runtime.processors.CallProcessor processor = new org.webharvest.runtime.processors.CallProcessor(null, configuration, scraper);
+    org.webharvest.runtime.ScraperContext functionContext = processor.getFunctionContext();
+
+    org.webharvest.definition.FunctionDef functionDef = scraper.getConfiguration().getFunctionDef(functionName);
+    if (functionDef == null) {
+        throw new RuntimeException("Function \"" + functionName + "\" is undefined!");
     }
-    catch (org.webharvest.exception.PluginException e) {
-        throw new RuntimeException("Exception during execution of <function name=\"${functionName}\" /> (params=${params})", e);
+
+    org.webharvest.runtime.processors.CallProcessor runningFunction = scraper.getRunningFunction();
+    org.webharvest.runtime.ScraperContext callerContext = runningFunction == null 
+        ? scraper.getContext() 
+        : runningFunction.getFunctionContext();
+
+    functionContext.putAll(callerContext);
+    for (Map.Entry<String, Object> param : functionParams.entrySet()) {
+        functionContext.put(param.getKey(), org.webharvest.utils.CommonUtil.createVariable(param.getValue()));
     }
-}
+
+    scraper.addRunningFunction(processor);
+
+    org.webharvest.runtime.variables.Variable functionResult = new org.webharvest.runtime.processors.BodyProcessor(functionDef).execute(scraper, functionContext);
+
+    scraper.removeRunningFunction();
+
+    return functionResult;
+};
